24f42e255382f7a24f81f721ca034dbaccac8e4d
najeesimmons <najee.bryant.simmons@gmail.com>
2023-06-26 09:16:49 -0400

    boilerplate

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -4,7 +4,9 @@ import type { AppProps } from 'next/app'
 
 export default function App({ Component, pageProps }: AppProps) {
   return (<>
-  <MantineProvider>
+  <MantineProvider withGlobalStyles
+      withNormalizeCSS
+      theme={{colorScheme: "light"}}>
   <Component {...pageProps} />
   </MantineProvider></>)
 }

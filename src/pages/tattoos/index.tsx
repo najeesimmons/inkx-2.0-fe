8e4421bd4c8dcc007a0c5e481abcb012017d160a
najeesimmons <najee.bryant.simmons@gmail.com>
2023-06-26 16:34:11 -0400

    feteching tattoo data

--- src/pages/tattoos/index.tsx
+++ src/pages/tattoos/index.tsx
@@ -1,4 +1,29 @@
-import { Text } from "@mantine/core"
+import { Text, Image, LoadingOverlay} from "@mantine/core"
+import { useEffect, useState } from "react";
 
-export default function Artist() {
-    return (<Text>This will be the tattoos page</Text>)}
+export default function Tattoos() {
+    const [tattoos, setTattoos] = useState([]);
+    const [tattoosIsLoading, setTattoosIsLoading] = useState(true);
+    
+    useEffect(() => {
+        const getTattoos = async () => {
+            try {
+            const response = await fetch('https://backend-api.tattoodo.com/api/v2/feeds/explore?&limit=24&page=1')
+            const data = await response.json()
+            setTattoos(data.data)
+            } catch (error) {
+                console.log(`getTattoos failed: ${error}`)
+        }}
+        if (tattoosIsLoading) getTattoos()
+        setTattoosIsLoading(false)
+    }, []);
+   
+    const fetchedTattoos = tattoos.map((tattoo) => {
+        return (
+            <Image key={tattoo.data.id} src={tattoo.data.image.url} alt={tattoo.data.description} width={300} height={300} />
+        )})
+
+        if (tattoosIsLoading) return <LoadingOverlay visible={tattoosIsLoading}/>
+    return (<>
+    {fetchedTattoos}
+    </>)}

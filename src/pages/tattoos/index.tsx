9ac5f1480b3407d9794dbb7cf208a24cb3f09bc7
najeesimmons <najee.bryant.simmons@gmail.com>
2023-06-26 17:20:38 -0400

    tattoo fetch req

--- src/pages/tattoos/index.tsx
+++ src/pages/tattoos/index.tsx
@@ -1,5 +1,7 @@
-import { Text, Image, LoadingOverlay} from "@mantine/core"
+import { Image, LoadingOverlay} from "@mantine/core"
 import { useEffect, useState } from "react";
+import Masonry from "react-masonry-css";
+import styles from '../../styles/Tattoos.module.css'
 
 export default function Tattoos() {
     const [tattoos, setTattoos] = useState([]);
@@ -8,7 +10,7 @@ export default function Tattoos() {
     useEffect(() => {
         const getTattoos = async () => {
             try {
-            const response = await fetch('https://backend-api.tattoodo.com/api/v2/feeds/explore?&limit=24&page=1')
+            const response = await fetch('https://backend-api.tattoodo.com/api/v2/feeds/explore?&limit=24&page=2')
             const data = await response.json()
             setTattoos(data.data)
             } catch (error) {
@@ -18,12 +20,26 @@ export default function Tattoos() {
         setTattoosIsLoading(false)
     }, []);
    
-    const fetchedTattoos = tattoos.map((tattoo) => {
+    const fetchedTattoos = tattoos.map((tattoo: any) => {
         return (
-            <Image key={tattoo.data.id} src={tattoo.data.image.url} alt={tattoo.data.description} width={300} height={300} />
+            <Image key={tattoo.data.id} src={tattoo.data.image.url} alt={tattoo.data.description} className={styles.my_masonry_grid_items} />
         )})
 
+        const breakpointColumnsObj = {
+            default: 4,
+            1100: 3,
+            700: 2,
+            500: 1
+          };
+
         if (tattoosIsLoading) return <LoadingOverlay visible={tattoosIsLoading}/>
+        
     return (<>
+    <Masonry
+        breakpointCols={breakpointColumnsObj}
+        className={styles.my_masonry_grid}
+        columnClassName={styles.my_masonry_grid_column}
+    >
     {fetchedTattoos}
+</Masonry>
     </>)}

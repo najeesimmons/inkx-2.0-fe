9c6acaf37e38e08199a9cdc845d4fd228a15ad68
najeesimmons <najee.bryant.simmons@gmail.com>
2023-06-26 09:02:56 -0400

    boilerplate

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,6 +1,11 @@
 import '@/styles/globals.css'
 import type { AppProps } from 'next/app'
+import Head from 'next/head'
 
 export default function App({ Component, pageProps }: AppProps) {
+  <Head>
+        <title>Page title</title>
+        <meta name="viewport" content="minimum-scale=1, initial-scale=1, width=device-width" />
+      </Head>
   return <Component {...pageProps} />
 }

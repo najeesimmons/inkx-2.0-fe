1301e5a5dee1f624bd64f293e262726f716c5ac0
najeesimmons <najee.bryant.simmons@gmail.com>
2023-06-26 09:12:44 -0400

    boilerplate

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,13 +1,9 @@
 import '@/styles/globals.css'
 import { MantineProvider } from '@mantine/core'
 import type { AppProps } from 'next/app'
-import Head from 'next/head'
 
 export default function App({ Component, pageProps }: AppProps) {
-  return (<><Head>
-    <title>inkX</title>
-    <meta name="viewport" content="minimum-scale=1, initial-scale=1, width=device-width" />
-  </Head>
+  return (<>
   <MantineProvider>
   <Component {...pageProps} />
   </MantineProvider></>)

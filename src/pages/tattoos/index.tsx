90f3ad64d67387c1f1158ac6888ab849f6eff477
najeesimmons <najee.bryant.simmons@gmail.com>
2023-06-27 09:08:59 -0400

    progress on infinite scroll

--- src/pages/tattoos/index.tsx
+++ src/pages/tattoos/index.tsx
@@ -1,5 +1,5 @@
 import { Image, LoadingOverlay, Text, createStyles} from "@mantine/core"
-import { useEffect, useState } from "react";
+import { useCallback, useEffect, useState } from "react";
 import Masonry from "react-masonry-css";
 import InfiniteScroll from "react-infinite-scroll-component";
 
@@ -24,23 +24,67 @@ export default function Tattoos() {
 
     const { classes } = useStyles();
     const [tattoos, setTattoos] = useState([]);
-    const [tattoosIsLoading, setTattoosIsLoading] = useState<boolean>(true);
-    const [totalPages, setTotalPages] = useState<number>(0);
-    const [page, setPage] = useState(1);
+    const [tattoosLoading, setTattoosLoading] = useState<boolean>(true);
+    const [page, setPage] = useState<number>(1);
+    const [isMore, setIsMore] = useState<boolean>(true);
+    const [error, setError] = useState<boolean>(false);
     
-    useEffect(() => {
-        const getTattoos = async () => {
-            try {
-            const response = await fetch(`https://backend-api.tattoodo.com/api/v2/feeds/explore?&limit=50&page=${page}`)
-            const data = await response.json()
-            setTattoos(data.data)
-            setTotalPages(data.meta.total_pages)
-            } catch (error) {
-                console.log(`getTattoos failed: ${error}`)
-        }}
-        if (tattoosIsLoading) getTattoos()
-        setTattoosIsLoading(false)
-    }, [page, tattoosIsLoading]);
+    const getTattoos = useCallback (
+      async (page: number) => {
+      try {
+        const response = await fetch(`https://backend-api.tattoodo.com/api/v2/feeds/explore?&limit=50&page=${page}`)
+        const data = await response.json()
+        const isMore = page !== data?.meta?.pagination?.total_pages;
+        return {
+          isMore,
+          data: data.data,
+          error: false,
+          nextPage: page + 1,
+        };
+      } catch (error) {
+        return {
+          isMore: false,
+          data: [],
+          error: true,
+          nextPage: page + 1,
+        };
+      }
+    },
+    []
+  );
+
+  const getMoreTattoos = async (page: number) => {
+    const { isMore, data, error, nextPage } = await getTattoos(page);
+
+    if (error) {
+      console.error("An error occurred");
+      setError(true);
+      return;
+    }
+
+    setTattoos((prev) => [...prev, ...data]);
+    setPage(nextPage);
+    setIsMore(isMore);
+  };
+
+  useEffect(() => {
+    async function fetchData() {
+      setTattoosLoading(true);
+      const { isMore, data, error, nextPage } = await getTattoos(1);
+
+      if (error) {
+        console.error("An error occurred");
+        setError(true);
+        return;
+      }
+      setTattoos(data);
+      setPage(nextPage);
+      setIsMore(isMore);
+      setTattoosLoading(false);
+    }
+
+    fetchData();
+  }, [getTattoos]);
    
     const fetchedTattoos = tattoos.map((tattoo: any) => {
         return (
@@ -54,20 +98,20 @@ export default function Tattoos() {
             500: 1
           };
 
-        if (tattoosIsLoading) return <LoadingOverlay visible={tattoosIsLoading}/>
+        if (tattoosLoading) return <LoadingOverlay visible={tattoosLoading}/>
         
     return (
             <>
               <InfiniteScroll
               dataLength={tattoos.length}
-              loader={<LoadingOverlay visible={tattoosIsLoading}/>}
-              hasMore={page < totalPages}
+              loader={<LoadingOverlay visible={tattoosLoading}/>}
+              hasMore={isMore}
               endMessage={
                   <p style={{ textAlign: 'center' }}>
                     <Text>There are no more tattoos to view.</Text>
                   </p>
                 }
-                next={() => setPage(page + 1)}
+                next={async () => await getMoreTattoos(page)}
               >
               <Masonry
                   breakpointCols={breakpointColumnsObj}

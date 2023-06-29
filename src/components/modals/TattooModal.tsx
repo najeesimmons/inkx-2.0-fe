605858e08a63338b8cf28ad2d990245612ca1c41
najeesimmons <najee.bryant.simmons@gmail.com>
2023-06-29 09:11:18 -0400

    skeleton for tattoo modal

--- src/components/modals/TattooModal.tsx
+++ src/components/modals/TattooModal.tsx
@@ -1,7 +1,16 @@
+import { Card, Group, Stack, Text, Title } from "@mantine/core";
+
 export default function TattooModal () {
   return (
-    <>
-      
-    </>
+      <Card>
+        <Group>
+        <Card.Section>
+        </Card.Section>
+       <Stack>
+        <Title>Tattoo Title</Title>
+        <Text>Artist Name</Text>
+       </Stack>
+        </Group>
+      </Card>
   );
 }

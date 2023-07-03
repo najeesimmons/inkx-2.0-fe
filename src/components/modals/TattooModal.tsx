4dc4e4e89165a8232e14cbc74c22e2fd32b0a8f3
najeesimmons <najee.bryant.simmons@gmail.com>
2023-07-02 21:28:44 -0400

    rudimentary carousel working

--- src/components/modals/TattooModal.tsx
+++ src/components/modals/TattooModal.tsx
@@ -1,6 +1,15 @@
-import { Card, Group, Modal, Stack, Text, Title, Image } from "@mantine/core";
-import { useDisclosure } from "@mantine/hooks";
+import {
+  Card,
+  Group,
+  Modal,
+  Stack,
+  Text,
+  Title,
+  Image,
+  rem,
+} from "@mantine/core";
 import { Carousel } from "@mantine/carousel";
+import { useState } from "react";
 
 interface TattooModalProps {
   opened: boolean;
@@ -9,22 +18,37 @@ interface TattooModalProps {
 }
 
 export function TattooModal({ opened, onClose, tattoos }: TattooModalProps) {
+  const [currentTattoo, setCurrentTattoo] = useState<{}>({});
+
+  const carouselItems = tattoos.map((tattoo: any) => (
+    <Carousel.Slide key={tattoo?.id}>
+      <Card>
+        <Group position="center">
+          <Card.Section>
+            <Image
+              src={tattoo?.imageUrl}
+              alt={tattoo?.description}
+              fit="contain"
+              height={500}
+            />
+          </Card.Section>
+          <Stack>
+            <Title order={3}>{tattoo.description}</Title>
+            <Title order={4}>{tattoo.artist.name}</Title>
+          </Stack>
+        </Group>
+      </Card>
+    </Carousel.Slide>
+  ));
+
   return (
     <Modal
       opened={opened}
       onClose={onClose}
       styles={{ root: { margin: "0 auto" } }}
+      fullScreen
     >
-      <Card>
-        <Group>
-          <Card.Section>{/* carousel of tattoos here */}</Card.Section>
-          <Stack>
-            <Title>Tattoo Title</Title>
-            <Text>Artist Name</Text>
-          </Stack>
-        </Group>
-        x
-      </Card>
+      <Carousel>{carouselItems}</Carousel>
     </Modal>
   );
 }

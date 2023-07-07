c345ff2911a5796d997bc93a1e39397b7f5582c2
najeesimmons <najee.bryant.simmons@gmail.com>
2023-07-07 11:34:47 -0400

    add breakpoints var to TattooMasonry

--- src/components/TattooMasonry.tsx
+++ src/components/TattooMasonry.tsx
@@ -4,6 +4,13 @@ import InfiniteScroll from "react-infinite-scroll-component";
 import Masonry from "react-masonry-css";
 
 export default function TattooMasonry() {
+  const breakpointColumnsObj = {
+    default: 4,
+    1100: 3,
+    700: 2,
+    500: 1,
+  };
+
   <InfiniteScroll
     dataLength={tattoos.length}
     loader={<LoadingOverlay visible={tattoosLoading} />}

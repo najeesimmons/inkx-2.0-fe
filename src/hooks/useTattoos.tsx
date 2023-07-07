f8b4ac70b40c977aa9b9bca9d18c082285005fe5
najeesimmons <najee.bryant.simmons@gmail.com>
2023-07-07 12:33:20 -0400

    moving state vars to reachable point

--- src/hooks/useTattoos.tsx
+++ src/hooks/useTattoos.tsx
@@ -1,10 +1,5 @@
 import { useState } from "react";
 
-const [isMore, setIsMore] = useState<boolean>(true);
-const [error, setError] = useState<boolean>(false);
-const [tattoos, setTattoos] = useState<{}[]>([]);
-const [tattoosLoading, setTattoosLoading] = useState<boolean>(true);
-
 const parseTattoos = (data: any) => {
   return data.map((d: any) => {
     return {
@@ -24,27 +19,46 @@ const parseTattoos = (data: any) => {
   });
 };
 
+// TODO: setPage to nextPage as returned from useTattoos wherever hook is implemented
+//   setPage(nextPage);
+
 // TODO: originally wrapped getTattos in useCallback before componetization, would it have any benefit here?
-export const getTattoos = async (page: number) => {
+const useTattoos = async (page: number, limit: number) => {
+  const [isMore, setIsMore] = useState<boolean>(true);
+  const [error, setError] = useState<boolean>(false);
+  const [tattoos, setTattoos] = useState<{}[]>([]);
+  const [tattoosLoading, setTattoosLoading] = useState<boolean>(true);
   try {
+    setTattoosLoading(true);
     const response = await fetch(
       `https://backend-api.tattoodo.com/api/v2/feeds/explore?&limit=${limit}&page=${page}`
     );
     const data = await response.json();
     const isMore = page !== data?.meta?.pagination?.total_pages;
     const parsedTattoos = parseTattoos(data.data);
+    setTattoos((prevTattoos) => [...prevTattoos, ...parsedTattoos]);
+    // TODO: curious about need to use useState to setIsMore, rather than just return isMore var
+    setIsMore(isMore);
+    setTattoosLoading(false);
     return {
-      isMore,
-      data: parsedTattoos,
+      isMore: isMore,
+      tattoos: tattoos,
+      tattoosLoading: tattoosLoading,
       error: false,
       nextPage: page + 1,
     };
   } catch (error) {
+    setError(true);
+    console.log("error:", error);
+    setTattoosLoading(false);
     return {
       isMore: false,
-      data: [],
+      tattoos: tattoos,
+      tattoosLoading: tattoosLoading,
       error: true,
       nextPage: page + 1,
     };
   }
 };
+
+export { useTattoos };
